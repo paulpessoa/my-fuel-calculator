dd361278ac4251d0bdc5a0cc42543b4f8336f171
Paul Pessoa <paulmspessoa@gmail.com>
2023-04-15 16:25:18 -0300

    ajustes em fuelcalculator

--- src/components/FuelCalculator.vue
+++ src/components/FuelCalculator.vue
@@ -1,51 +1,44 @@
 <template>
     <v-container class="d-flex align-center justify-center">
-        <v-card width="400">
-            <v-card-title ali>Calculadora de Combustível</v-card-title>
-            <v-card-text>
-                <v-col>
-                    <v-text-field v-model="distance" label="Distância percorrida (km)"></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field v-model="price" label="Preço do combustível (R$)"></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field v-model="consumption" label="Consumo médio (km/L)"></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field v-model="capacity" label="Capacidade do tanque (L)"></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field v-model="autonomy" label="Autonomia (km)"
-                        hint="Distância máxima que pode ser percorrida com o tanque cheio"></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field v-model="fuelCost" label="Custo do combustível (R$)"
-                        hint="Custo total do combustível para percorrer a distância informada"></v-text-field>
-                </v-col>
-            </v-card-text>
-        </v-card>
+      <v-card title="Calculadora de Combustível" class="pa-6">
+        <v-text-field variant="outlined" v-model.number="distance" label="Distância percorrida" suffix="km" class="pt-6"></v-text-field>
+        <v-text-field variant="outlined" v-model.number="price" label="Preço do combustível"></v-text-field>
+        <v-text-field variant="outlined" v-model.number="consumption" label="Consumo médio" suffix="km/L"></v-text-field>
+        <v-text-field variant="outlined" v-model.number="capacity" label="Capacidade do tanque" suffix="Litros"></v-text-field>
+        <v-card-actions>
+          <v-col>
+            <v-text-field variant="outlined" class="py-4" v-model.number="autonomy" label="Autonomia" suffix="km" hint="Distância máxima com o tanque cheio"></v-text-field>
+            <v-text-field variant="outlined" v-model.number="fuelCost" label="Custo do combustível" hint="Custo total para a distância informada"></v-text-field>
+          </v-col>
+        </v-card-actions>
+      </v-card>
     </v-container>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: 'FuelCalculator',
     data() {
-        return {
-            distance: 100,
-            price: 5.5,
-            consumption: 15,
-            capacity: 45,
-        };
+      return {
+        distance: 100,
+        price: 210,
+        consumption: 15,
+        capacity: 45,
+      };
     },
     computed: {
-        autonomy() {
-            return this.consumption && this.capacity ? Math.round(this.consumption * this.capacity) : 0;
-        },
-        fuelCost() {
-            return this.distance && this.consumption && this.price ? (this.distance / this.consumption) * this.price : 0;
-        },
+        formattedPrice() {
+      return this.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     },
-};
-</script>
+      autonomy() {
+        return this.consumption && this.capacity ? Math.round(this.consumption * this.capacity) : 0;
+      },
+      fuelCost() {
+        let cost = this.distance && this.consumption && this.price ? (this.distance / this.consumption) * this.price : 0;
+        return cost.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+      },
+    },
+    methods: {},
+  };
+  </script>
+  
