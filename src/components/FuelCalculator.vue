253d1916b0a9a49a8a199a1f34d443ff5d25e2b2
Paul Pessoa <paulmspessoa@gmail.com>
2023-04-15 16:52:44 -0300

    adicionando google api

--- src/components/FuelCalculator.vue
+++ src/components/FuelCalculator.vue
@@ -1,6 +1,27 @@
 <template>
     <v-container class="d-flex align-center justify-center">
       <v-card title="Calculadora de Combustível" class="pa-6">
+        <v-text>
+            <v-autocomplete
+            v-model="startAddress"
+            :items="startAddresses"
+            label="Endereço de partida"
+            item-text="description"
+            item-value="place_id"
+            placeholder="Digite o endereço de partida"
+            @input="getStartAddressSuggestions"
+          ></v-autocomplete>
+          <v-autocomplete
+            v-model="endAddress"
+            :items="endAddresses"
+            label="Endereço de destino"
+            item-text="description"
+            item-value="place_id"
+            placeholder="Digite o endereço de destino"
+            @input="getEndAddressSuggestions"
+          ></v-autocomplete>
+        </v-text>
+
         <v-text-field variant="outlined" v-model.number="distance" label="Distância percorrida" suffix="km" class="pt-6"></v-text-field>
         <v-text-field variant="outlined" v-model.number="price" label="Preço do combustível"></v-text-field>
         <v-text-field variant="outlined" v-model.number="consumption" label="Consumo médio" suffix="km/L"></v-text-field>
@@ -14,31 +35,58 @@
       </v-card>
     </v-container>
   </template>
-  
-  <script>
-  export default {
-    name: 'FuelCalculator',
-    data() {
-      return {
-        distance: 100,
-        price: 210,
-        consumption: 15,
-        capacity: 45,
-      };
-    },
-    computed: {
-        formattedPrice() {
+
+<script>
+import axios from 'axios';
+const apiKey = process.env.VUE_APP_API_KEY;
+
+export default {
+  name: 'FuelCalculator',
+  data() {
+    return {
+      distance: 100,
+      price: 210,
+      consumption: 15,
+      capacity: 45,
+      startAddress: null,
+      endAddress: null,
+      startAddresses: [],
+      endAddresses: [],
+      apiKey: apiKey,
+    };
+  },
+  computed: {
+    formattedPrice() {
       return this.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     },
-      autonomy() {
-        return this.consumption && this.capacity ? Math.round(this.consumption * this.capacity) : 0;
-      },
-      fuelCost() {
-        let cost = this.distance && this.consumption && this.price ? (this.distance / this.consumption) * this.price : 0;
-        return cost.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-      },
+    autonomy() {
+      return this.consumption && this.capacity ? Math.round(this.consumption * this.capacity) : 0;
     },
-    methods: {},
-  };
-  </script>
-  
+    fuelCost() {
+      let cost = this.distance && this.consumption && this.price ? (this.distance / this.consumption) * this.price : 0;
+      return cost.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    },
+  },
+  methods: {
+    async getStartAddressSuggestions(query) {
+        console.log("seila", query)
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&types=(cities)&key=${apiKey}`);
+            this.startAddresses = response.data.predictions;
+        },
+        async getEndAddressSuggestions(query) {
+        console.log("seila", query)
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&types=(cities)&key=${apiKey}`);
+            this.endAddresses = response.data.predictions;
+        },
+        async calculateDistance() {
+            try {
+                const response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${this.startAddress}&destinations=${this.endAddress}&key=${apiKey}`);
+                    const distanceInMeters = response.data.rows[0].elements[0].distance.value;
+                    this.distance = distanceInMeters / 1000;
+                } catch (error) {
+                    console.error(error);
+                }
+    },
+  },
+};
+</script>
